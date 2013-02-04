61343aaa1a4271af0203ba6052d2a66fc646bf85
Simon H. Eskildsen <sirup@sirupsen.com>
2013-02-04 19:29:57 +0100

    Add support for multiple callbacks

--- test/test-callbacks.js
+++ test/test-callbacks.js
@@ -1,6 +1,6 @@
 var SteakMachine = require("../lib/steak-machine.js");
 
-exports.callbacks = {
+exports["Simple callbacks"] = {
   setUp: function(callback) {
     var self = this;
     self.events = [];
@@ -47,6 +47,72 @@ exports.callbacks = {
 
     test.equal(this.events[1], "before");
 
+    test.done();
+  }
+}
+
+exports["Multiple callbacks"] = {
+  setUp: function(callback) {
+    var self = this;
+    self.events = [];
+
+    self.tester = {
+      stateMachine: new SteakMachine([
+         {
+          from: "A",
+          to: "B"
+        },
+        {
+          from: "B",
+          to: "C",
+          after: [
+            function() { self.events.push("after 1"); }, 
+            function() { self.events.push("after 2"); }
+          ]
+        },
+        {
+          from: "C",
+          to: "D",
+          before: function() {
+            self.events.push("before");
+          }
+        }
+      ], this)
+    }
+
+    callback();
+  },
+
+  "multiple after callbacks should all be fired in order": function(test) {
+    this.tester.stateMachine.next(); // A -> B
+    this.tester.stateMachine.next(); // B -> C
+
+    test.equal(this.events[0], "after 1");
+    test.equal(this.events[1], "after 2");
+
+    test.done();
+  }
+}
+
+exports["Invalid callback type"] = {
+  setUp: function(callback) {
+    var self = this;
+
+    self.tester = {
+      stateMachine: new SteakMachine([
+         {
+          from: "A",
+          after: "invalid",
+          to: "B"
+        },
+      ], this)
+    }
+
+    callback();
+  },
+
+  "results in error when called": function(test) {
+    test.throws(function(){this.tester.stateMachine.next()});
     test.done();
   }
 }
